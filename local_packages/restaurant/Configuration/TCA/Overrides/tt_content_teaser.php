660e54d1679c58f3aed992a4012d58eb116362c8
davidferro <david.ferro@digital-zombies.com>
2024-04-09 15:29:28 +0100

    [FEATURE] Integrate the backend of Teaser element

---
+++ local_packages/restaurant/Configuration/TCA/Overrides/tt_content_teaser.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['label'] = 'LLL:EXT:restaurant/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser';
+$GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['description'] = 'LLL:EXT:restaurant/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser.description';
