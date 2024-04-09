aaa65ac57e766f5aed43c07b41a63e9c3506b93c
davidferro <david.ferro@digital-zombies.com>
2024-04-09 16:38:44 +0100

    [TASK] Structure TCA

--- local_packages/restaurant/Configuration/TCA/Overrides/tt_content_teaser.php
+++ local_packages/restaurant/Configuration/TCA/Overrides/tt_content_teaser.php
@@ -1,4 +1,9 @@
 <?php
+defined('TYPO3') || die();
 
-$GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['label'] = 'LLL:EXT:restaurant/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser';
-$GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['description'] = 'LLL:EXT:restaurant/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser.description';
+call_user_func(
+    static function ($extensionKey) {
+        $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['label'] =  'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser';
+        $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['description'] =  'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser.description';
+    }, 'restaurant'
+);
