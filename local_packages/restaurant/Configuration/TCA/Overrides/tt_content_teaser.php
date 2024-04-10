ff301aecac6526fcda7ef746a78ed11744818f2a
davidferro <david.ferro@digital-zombies.com>
2024-04-10 16:36:12 +0100

    [TASK] Set image min and maxItem 1 (required and max 1) in Teaser element

--- local_packages/restaurant/Configuration/TCA/Overrides/tt_content_teaser.php
+++ local_packages/restaurant/Configuration/TCA/Overrides/tt_content_teaser.php
@@ -3,7 +3,9 @@ defined('TYPO3') || die();
 
 call_user_func(
     static function ($extensionKey) {
-        $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['label'] =  'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser';
-        $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['description'] =  'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser.description';
+        $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['label'] = 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser';
+        $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items']['2']['description'] = 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.textpic.teaser.description';
+        $GLOBALS['TCA']['tt_content']['types']['textpic']['columnsOverrides']['image']['config']['minitems'] = 1;
+        $GLOBALS['TCA']['tt_content']['types']['textpic']['columnsOverrides']['image']['config']['maxitems'] = 1;
     }, 'restaurant'
 );
