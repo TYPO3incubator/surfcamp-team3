5538863e1630f9af2457b907937f8c844c345620
Łukasz Uznański <l.uznanski@macopedia.com>
2024-04-11 18:22:57 +0100

    [FEATURE] Add base contact form rendering

--- local_packages/restaurant/ext_localconf.php
+++ local_packages/restaurant/ext_localconf.php
@@ -2,3 +2,26 @@
 defined('TYPO3') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['restaurant'] = 'EXT:restaurant/Configuration/RTE/Default.yaml';
+
+call_user_func(static function () {
+
+    // Add module configuration
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        'plugin.tx_form {
+    settings {
+        yamlConfigurations {
+            100 = EXT:restaurant/Configuration/Sets/Restaurant/Form/Setup.yaml
+            130 = EXT:restaurant/Configuration/Sets/Restaurant/Form/FormDefinition/Contactform.form.yaml
+        }
+    }
+}
+module.tx_form {
+    settings {
+        yamlConfigurations {
+            100 = EXT:restaurant/Configuration/Sets/Restaurant/Form/Setup.yaml
+            130 = EXT:restaurant/Configuration/Sets/Restaurant/Form/FormDefinition/Contactform.form.yaml
+        }
+    }
+}'
+    );
+});
