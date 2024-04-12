bf2ac826ae1b92fbd429307b71e39fd924a4714b
Łukasz Uznański <l.uznanski@macopedia.com>
2024-04-12 16:37:52 +0100

    Render all products

--- local_packages/restaurant/Classes/Controller/ProductController.php
+++ local_packages/restaurant/Classes/Controller/ProductController.php
@@ -5,21 +5,22 @@
 namespace Surfcamp\Restaurant\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use Surfcamp\Restaurant\Domain\Repository\ProductRepository;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class ProductController extends ActionController
 {
-    public function __construct() {
-
-    }
+//    private ProductRepository $productRepository;
+    public function __construct(
+        private readonly ProductRepository $productRepository,
+    ) {}
 
     public function listAction(): ResponseInterface
     {
         $this->view->assign(
-            'test',
-            'test',
+            'products',
+            $this->productRepository->findAll()
         );
-
 
         return $this->htmlResponse();
     }
