ceb0333a5ae2da46648b531149fa87ba104b7579
kaigrosz <info@kai-grosz.de>
2024-04-12 18:17:38 +0100

    WIP templating

--- local_packages/restaurant/Classes/Controller/ProductController.php
+++ local_packages/restaurant/Classes/Controller/ProductController.php
@@ -6,6 +6,9 @@ namespace Surfcamp\Restaurant\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Surfcamp\Restaurant\Domain\Repository\ProductRepository;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\DebugUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class ProductController extends ActionController
@@ -13,13 +16,27 @@ class ProductController extends ActionController
 //    private ProductRepository $productRepository;
     public function __construct(
         private readonly ProductRepository $productRepository,
+        private readonly ConnectionPool $connectionPool
     ) {}
 
     public function listAction(): ResponseInterface
     {
-        $this->view->assign(
-            'products',
-            $this->productRepository->findAll()
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable('tt_content');
+        $result = $queryBuilder
+            ->select('*')
+            ->from('sys_category')
+            ->executeQuery();
+
+        $categories = [];
+        while ($row = $result->fetchAssociative()) {
+            $categories[] = $row;
+        }
+
+        $this->view->assignMultiple(
+          [
+              'products' => $this->productRepository->findAll(),
+              'categories' => $categories
+          ]
         );
 
         return $this->htmlResponse();
