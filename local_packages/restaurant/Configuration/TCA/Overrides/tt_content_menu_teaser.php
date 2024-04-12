7d73008ac0e5058df9dc1b39d1804dc48734df26
davidferro <david.ferro@digital-zombies.com>
2024-04-12 14:02:49 +0100

    [FEATURE] Implement Menu Teaser backend element

---
+++ local_packages/restaurant/Configuration/TCA/Overrides/tt_content_menu_teaser.php
@@ -0,0 +1,80 @@
+<?php
+defined('TYPO3') || die();
+
+call_user_func(
+    static function ($extensionKey) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+            [
+                'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.menu_teaser',
+                'menu_teaser',
+                'content-menu-pages',
+                'default',
+                'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.menu_teaser.description',
+            ],
+            'CType',
+            'restaurant'
+        );
+
+        $temporaryColumn = [
+            'menu_items' => [
+                'label' => 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.type.menu_teaser.items',
+                'config' => [
+                    'type' => 'inline',
+                    'foreign_table' => 'tt_content',
+                    'CType' => [
+                        'menu_items' => [
+                            'config' => [
+                                'overrideChildTca' => [
+                                    'columns' => [
+                                        'colPos' => [
+                                            'config' => [
+                                                'default' => 999
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                                'items' => [
+                                    [
+                                        'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:CType.header',
+                                        'description' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:CType.header.description',
+                                        'value' => 'header',
+                                        'icon' => 'content-header',
+                                        'group' => 'default',
+                                    ],
+                                ],
+                                'default' => 'header',
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ];
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+            'tt_content',
+            $temporaryColumn
+        );
+
+        $GLOBALS['TCA']['tt_content']['types']['menu_teaser'] = [
+            'showitem' => ' --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                   --palette--;;general,
+                   header,
+                   header_link,
+                   subheader,
+                   menu_items,
+                   --palette--;;language,
+                   --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                   --palette--;;hidden,
+                   --palette--;;access,
+           ',
+        ];
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+            'tt_content',
+            'menu_teaser',
+            '',
+            'after:header'
+        );
+
+    }, 'restaurant'
+);
